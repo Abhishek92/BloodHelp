4e6e1f1b2d085ba6933b692d608cbd99aa75d6d4
abhishek <abhishek.kotiyal@ranosys.com>
2016-01-03 20:32:48 +0530

    Commit changes after added facebook authentication and google places api

--- app/src/main/java/com/android/bloodhelp/MainActivity.java
+++ app/src/main/java/com/android/bloodhelp/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
+
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -27,17 +33,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.login_button)
-    void loginWithFacebook()
-    {
-        ParseFacebookUtils.logInWithReadPermissionsInBackground(this, Arrays.asList("public_profile"), new LogInCallback() {
+    void loginWithFacebook() {
+        ParseFacebookUtils.logInWithReadPermissionsInBackground(this, Arrays.asList("public_profile","user_location","user_birthday"), new LogInCallback() {
             @Override
             public void done(ParseUser user, ParseException err) {
                 if (user == null) {
                     Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
                 } else if (user.isNew()) {
+                    getUserInfoFromFacebook();
                     Log.d("MyApp", "User signed up and logged in through Facebook!");
+
                 } else {
                     Log.d("MyApp", "User logged in through Facebook!");
+                    getUserInfoFromFacebook();
                 }
             }
         });
@@ -47,5 +55,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void getUserInfoFromFacebook() {
+        GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                // Application code
+                System.out.println(object);
+            }
+        }).executeAsync();
     }
 }
